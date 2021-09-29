b8d0fee50daea8d94f39ea80f84e4aeae78ef3ba
shi-n <shi-n_xyz@cloudsquare.jp>
2021-09-29 20:31:43 +0900

    Initial Commit

---
+++ ExampleStudyMusicKit/ExampleStudyMusicKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleStudyMusicKitApp.swift
+//  ExampleStudyMusicKit
+//
+//  Created by Shin Inaba on 2021/09/29.
+//
+
+import SwiftUI
+
+@main
+struct ExampleStudyMusicKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
