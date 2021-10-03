68b73c7fcf1e270ccc42477fbf7a8c4a0c655206
shi-n <shi-n_xyz@cloudsquare.jp>
2021-10-03 21:29:03 +0900

    Status action.

--- ExampleStudyMusicKit/ExampleStudyMusicKitApp.swift
+++ ExampleStudyMusicKit/ExampleStudyMusicKitApp.swift
@@ -23,25 +23,32 @@ struct ExampleStudyMusicKitApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate {
     /// Opens a URL using the appropriate system service.
     @Environment(\.openURL) private var openURL
+    
+    var status = MusicAuthorization.Status.notDetermined
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        Task {
-            var status = MusicAuthorization.currentStatus
-            print("MusicAuthorization.Status:\(status)")
-            switch status {
-            case .notDetermined:
-                status = await MusicAuthorization.request()
-            case .denied:
-                if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
-                    openURL(settingsURL)
-                }
-            default:
-                break
+        self.status = MusicAuthorization.currentStatus
+        print("MusicAuthorization.Status:\(status)")
+        switch status {
+        case .notDetermined:
+            Task {
+                let status = await MusicAuthorization.request()
+                self.status = status
             }
-            print("MusicAuthorization.Status:\(status)")
+        case .denied:
+            if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
+                openURL(settingsURL)
+            }
+        case .restricted:
+            print("No request!!")
+        case .authorized:
+            print("Authorized!!")
+        @unknown default:
+            print("Future status!!")
         }
+        print("MusicAuthorization.Status:\(self.status)")
 
         return true
     }
