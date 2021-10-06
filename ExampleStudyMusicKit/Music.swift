6c154dd2f09273c19d6ab54667d73e11cd009e70
shi-n <shi-n_xyz@cloudsquare.jp>
2021-10-06 20:25:40 +0900

    add Example

--- ExampleStudyMusicKit/Music.swift
+++ ExampleStudyMusicKit/Music.swift
@@ -27,6 +27,8 @@ class Music: ObservableObject {
     
     func example() {
         Task() {
+            
+            print("\nExample 1 --------------------")
             let request = MusicCatalogSearchRequest(term: "氷室", types: [Artist.self])
             let response = try await request.response()
             print(response.debugDescription)
@@ -34,6 +36,27 @@ class Music: ObservableObject {
             artists.forEach({
                 print($0)
             })
+
+            print("\nExample 2 --------------------")
+            var request2 = MusicCatalogSearchRequest(term: "氷室京介", types: [Song.self])
+            request2.limit = 25
+            let response2 = try await request2.response()
+//            print(response2.debugDescription)
+            let songs: MusicItemCollection<Song> = response2.songs
+            songs.forEach({
+                print($0)
+            })
+
+            print("\nExample 3 --------------------")
+            var request3 = MusicCatalogSearchRequest(term: "氷室京介", types: [Song.self])
+            request3.limit = 100
+            let response3 = try await request3.response()
+            print(response3.debugDescription)
+
+            print("\nExample 4 --------------------")
+            let request4 = MusicCatalogSearchRequest(term: "氷室京介", types: [])
+            let response4 = try await request4.response()
+            print(response4.debugDescription)
         }
     }
 }
